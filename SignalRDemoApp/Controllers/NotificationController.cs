8453a72ae6ef2623db01818e07ffed4d5b2d68a3
Deepak Gupta <deepakgkr8@gmail.com>
2021-08-22 20:52:26 +0530

    SignalRAPPAspnetcore31

---
+++ SignalRDemoApp/Controllers/NotificationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRDemoApp.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRDemoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<NotificationHub> _notificationHub;
+        public NotificationController(IHubContext<NotificationHub> notificationHub)
+        {
+            _notificationHub = notificationHub;
+        }
+
+        [HttpPost("SendMessage")]
+        public void SendMessage(string message)
+        {
+            if (!string.IsNullOrEmpty(message)) {
+                _notificationHub.Clients.All.SendAsync("MESSAGE_FROM_SERVER", message);
+            }
+        }
+
+    }
+}
